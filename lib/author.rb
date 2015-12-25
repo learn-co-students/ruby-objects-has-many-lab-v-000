8d333db5291f9af3aa6003c708d923a4ec96bfec
jsgoldb <jgoldb@yahoo.com>
2015-12-25 10:48:59 -0600

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+attr_accessor :posts, :name
+
+@@post_count = 0
+
+def initialize(name)
+  @posts = []
+  @name = name
+end
+
+def add_post(post)
+  posts << post
+  post.author = self
+  @@post_count += 1
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  posts << post
+  post.author = self
+  @@post_count += 1
+end
+
+def self.post_count
+  @@post_count
+end
+
+end
