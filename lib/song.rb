59cc908a6d8fd1364635187e41043f25b77ea032
Rawradin <edgar.rawr@gmail.com>
2016-01-07 19:52:12 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if(self.artist == nil)
+      return nil
+    else
+      self.artist.name
+    end
+  end
+end
