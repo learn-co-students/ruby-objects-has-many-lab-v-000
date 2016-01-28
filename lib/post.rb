3061cbe57b5e3651bc61476dd80b0c75f894f34c
alexbarron <alexbarron@gmail.com>
2016-01-27 18:57:44 -0800

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author_name
+    if self.author.nil?
+      return nil
+    else
+      return self.author.name
+    end
+  end
+
+end
