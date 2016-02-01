e845a95717092ea289b907167b433c8bf03bf12a
glee38 <glee3388@gmail.com>
2016-01-31 20:08:55 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+  attr_accessor :author, :title
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if defined?(self.author.name)
+      self.author.name
+    else
+      nil
+    end
+  end
+end
