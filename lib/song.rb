f8a204273d01f4cc34918fcc6410225087f187ab
Richard-Frost <frost.rchrd@gmail.com>
2016-02-02 19:49:27 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+
+
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if @artist.nil?
+      nil
+    else
+      self.artist.name
+    end
+  end
+
+end
+
