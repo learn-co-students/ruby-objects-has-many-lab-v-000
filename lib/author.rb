45a8fb422c3b7278ff5ff241557ca3e2a70c7161
evanscloud <dijkstrascloud@gmail.com>
2016-02-05 16:47:32 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Author
+  attr_accessor :author
+
+  @@post_count = 0
+
+  def initialize(author)
+    @author = author
+    @posts = []
+  end
+
+  def name
+    @author
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post)
+    title = Post.new(post)
+    @posts << title
+    title.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
