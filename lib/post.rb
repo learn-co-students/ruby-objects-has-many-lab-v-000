c33943f02d8232df5f088d8e60137ae0335071d9
wsaxe <geosaxe@gmail.com>
2016-02-18 20:01:05 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+class Post
+  attr_accessor :author, :title
+  def initialize(title)
+    @title = title
+  end
+  def author_name
+    if defined? self.author.name
+      self.author.name
+    end
+  end
+end
