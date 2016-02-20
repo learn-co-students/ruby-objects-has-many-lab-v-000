f7b2ec29b065b6ecdabf7375fb7a1c6f1bd2209a
ShiLi007 <omgshi888@gmail.com>
2016-02-19 20:11:05 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+    attr_accessor :author,:title
+ 
+    def initialize(title)
+        @title = title
+    end
+
+    def author_name
+    @author == nil ? nil : self.author.name
+    
+  end
+end
+
