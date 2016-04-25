bdfb373a91711f14f2f9900afab2d6035dafd7a3
Michael Randell <michael.james.randell@gmail.com>
2016-04-25 13:32:29 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.name = self
+
+  end
+
+
+
+end
