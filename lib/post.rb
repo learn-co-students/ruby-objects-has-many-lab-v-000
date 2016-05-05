d2209ad99ba7160f23c1d4c9e46ff38d0b47947e
WilliamMena <williammena1991@gmail.com>
2016-05-05 19:41:04 -0400

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,18 @@
 class Post
 
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else
+      self.author.name
+    end
+  end
 
 
 
