92d65d9c8763ce67befcccfbecbec7194ad841c8
kaileeagray <kaileeagray@gmail.com>
2016-05-05 21:29:34 -0500

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -25,38 +25,5 @@ class Author
   def self.post_count
     @@post_count
   end
-
-  # def add_song(song)
-  #   @songs << song
-  #   song.artist = self
-  #   @@song_count += 1
-  # end
-  #
-  # def songs
-  #   @songs.dup.freeze
-  # end
-  #
-  # def self.songs
-  #   @songs
-  # end
-  #
-  # def add_song_by_name(name)
-  #    new_song = Song.new(name)
-  #    @songs << new_song
-  #    new_song.artist = self
-  #    @@song_count += 1
-  #  end
-
-
-  # describe "#add_post" do
-  #   it "takes in an argument of a post and adds that post to the author's collection and tells the post that it belongs to that author" do
-  #     hello_world = Post.new("Hello World")
-  #     betty.add_post(hello_world)
-  #     expect(betty.posts).to include(hello_world)
-  #     expect(hello_world.author).to eq(betty)
-  #   end
-  # end
-
-
-
+  
 end
