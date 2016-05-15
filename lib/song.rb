b3ffc7df8f84d3cbfa6d61783e39e9a5c5d13aab
zszalas2 <zszalas@gmail.com>
2016-05-15 12:18:21 -0400

    done and done

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Song
+  attr_accessor :artist, :name, :genre
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+   if @artist
+       self.artist.name
+    else
+        nil
+     end
+
+  end
+
+
+
+
+end
