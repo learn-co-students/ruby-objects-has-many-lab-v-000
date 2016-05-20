d2b060bfefc3dbe8ce296a27d7acfcdb73fcc69f
joelbitart1986 <joelbitar1986@gmail.com>
2016-05-19 23:07:55 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Artist
+	
+	attr_accessor :name
+	
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+	
+	def songs
+		@songs
+	end
+	
+	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
+	
+	def add_song_by_name(name)
+		song = Song.new(name)
+		self.songs << song
+		song.artist = self
+	end
+	
+end
