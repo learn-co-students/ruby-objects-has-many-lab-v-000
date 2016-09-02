fc98cbfd67ed99eb243ba4ff9e527bd32dca7598
Clare <clare.e.horton@gmail.com>
2016-09-02 18:11:58 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+    attr_accessor:name
+    @@song_count = 0
+    
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+    
+    def add_song(song)
+        self.songs << song
+        song.artist = self
+        @@song_count += 1
+    end
+    
+    def add_song_by_name(song_name)
+        song = Song.new(song_name)
+        song.artist = self
+        self.songs << song
+        @@song_count += 1
+    end
+    
+    def songs
+        @songs
+    end
+    
+    def self.song_count
+        @@song_count
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+    attr_accessor:name, :artist
+    def initialize(name)
+        @name = name
+    end
+    
+    def artist_name
+        if self.artist
+            return self.artist.name
+        else
+            return nil
+        end
+    end
+end
