dfc4d559aeb82830701bf538666defc2bedd8fbb
smithWEBtek <brad@smithwebtek.com>
2016-09-06 16:39:05 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,14 +6,6 @@ class Song
     @name = name
   end
 
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def artist
-    @artist
-  end
-
   def artist_name
     if artist;
     self.artist.name
