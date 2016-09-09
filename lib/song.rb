8871e499650ac9b38dfc40fefc2807d069363044
rgylling <mikegylling@yahoo.com>
2016-09-08 18:44:10 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+
+  attr_accessor :artist
+
+  def initialize(name)
+      @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def artist_name
+    if self.artist
+    self.artist.name
+    else
+    nil
+    end
+  end
+
+end
