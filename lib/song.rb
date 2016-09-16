0ef3e248ca1408e718291efdf0198bd727a29dc5
Adam Cooper <amcooper@gmail.com>
2016-09-16 18:30:36 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+	attr_accessor :artist
+	attr_reader :name
+
+  def initialize(name)
+  	@name = name
+  end
+
+  def artist_name
+  	if self.artist
+  		self.artist.name
+  	else
+  		nil
+  	end
+  end
+
+end
