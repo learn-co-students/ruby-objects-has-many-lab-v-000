6ae8871e1dde6eec4459dac24bda1664602b5b72
Ray Mulligan <rmullig2@optonline.net>
2016-10-14 01:35:38 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+
+  attr_accessor :author, :title
+
+  def initialize(title)
+    self.title = title
+    self.author = nil
+  end
+
+  def author_name
+    @author == nil ? nil : @author.name
+  end
+
+end
