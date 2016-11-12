5e7dc80ca8598ec9f5fbfd26228b4228463f86a2
Mary Umoh <umohm12@gmail.com>
2016-11-12 19:30:11 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,13 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
+    #@@song_count += 1
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
 end
 
   def add_song_by_name(name)
