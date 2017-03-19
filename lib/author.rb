eb1fa0495c8ea749bb2ac417f77f6878eac39b94
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-03-19 22:35:44 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -12,14 +12,14 @@ class Author
   end
 
   def add_post(post)
-    @posts << post
+    self.posts << post
     post.author = self
     @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
+    self.posts << post
     post.author = self
     @@post_count += 1
   end
