f8700562551ebceea2ebe790585e36bd77d9d607
Eva Navarror <emanao@me.com>
2017-04-10 02:32:23 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,27 @@
 class Artist
-  attr_accessor :song
-  def initialize
-    song=[]
+  attr_accessor :name, :songs
+  @@song_count=0
+
+  def initialize(name)
+    @name=name
+    @songs=[]
+  end
+
+  def add_song(song)
+    @songs<<song
+    song.artist=self
+    @@song_count+=1
+  end
+
+  def add_song_by_name(title)
+    song=Song.new(title)
+    @songs<<song
+    song.artist=self
+    @@song_count+=1
+  end
+
+  def self.song_count
+    @@song_count
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,13 @@
+#require "pry"
 class Song
-  attr_accessor :artist
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name=name
+  end
+
+  def artist_name
+    self.artist.name if self.artist!=nil
+  end
+
 end
