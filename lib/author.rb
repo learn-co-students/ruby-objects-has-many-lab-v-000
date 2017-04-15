bf056427bdc1a4acaaa44528f69452c05f474961
Siobhan Martin <bklyngal1994@gmail.com>
2017-04-15 19:14:12 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+
+   @@post_count = 0
+   attr_reader :name, :posts
+
+   def initialize(name)
+     @name = name
+     @posts = []
+   end
+
+   def add_post(post)
+     posts << post
+     post.author = self
+     @@post_count += 1
+   end
+
+   def add_post_by_title(post_title)
+     post = Post.new(post_title)
+     self.add_post(post) #explicit receiver. In the Artist class there is implicit receiver
+   end
+
+   def self.post_count
+     @@post_count
+   end
+
+ end
