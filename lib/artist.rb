def239f13a9136261020a75686b5aac4c49c50cb
Will-Sauer <will@heueso.com>
2017-04-30 17:53:33 -0700

    finished

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+attr_accessor :name, :songs
+@@song_count = 0
+
+def initialize (name)
+@name =  name
+@songs = []
+end
+
+
+def add_song (song)
+song.artist = self
+@songs << song
+@@song_count += 1
+
+end
+
+def add_song_by_name(song_name)
+new_song = Song.new(song_name)
+new_song.artist = self
+@songs << new_song
+@@song_count += 1
+end
+
+
+def self.song_count
+@@song_count    
+end
+
+end
