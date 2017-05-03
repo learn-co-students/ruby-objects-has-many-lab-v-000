d3a9fb0a49bc821a9f3156da068784795cc64272
Monika Williams <monika.m.williams@gmail.com>
2017-05-03 13:18:20 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,9 +20,9 @@ class Artist
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
+    name = Song.new(name)
+    @songs << name
+    name.artist = self
     @@song_count += 1
   end
 
