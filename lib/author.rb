0f62e65b0a56503422e0790594edb510ec8005bb
Ninian Bond <gentile.bond@gmail.com>
2017-06-07 22:23:33 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+  @@post_count = 0
+  attr_accessor :name, :posts
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @@post_count += 1
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    @@post_count += 1
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
