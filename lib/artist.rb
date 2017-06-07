8b7177607dcc5783e077db570b21654f15b7e5d7
Mel Chang <melanie.a.chang@gmail.com>
2017-06-07 23:45:27 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,11 @@
 class Artist
-  attr_accessor :name, :song
+  attr_accessor :name, :songs
 
   @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = []
-  end
-
-  def songs
-    @songs
   end
 
   def add_song(song)
--- lib/post.rb
+++ lib/post.rb
@@ -3,13 +3,13 @@ require 'pry'
 class Post
   attr_accessor :title, :author
 
-  def initialize(title)
+  def initialize(title)               #new posts are assigned titles
     @title = title
   end
 
   def author_name
-    if @author != nil
-      self.author.name
+    if @author != nil                #if author is not not assigned
+      self.author.name               #return this posts's author's name
     else
       nil
     end
