b9497c5d8ff8c422532108b49c3df9f56e7108c2
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-10 15:43:21 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+class Author
+  attr_accessor :name, :post, :author_name
+  @@post_count = 0
+
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    @@post_count += 1
+    post.author = self
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+  def author_name(post)
+    post.author_name = self
+  end
+
+end
