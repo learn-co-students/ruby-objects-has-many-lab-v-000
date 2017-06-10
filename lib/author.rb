e056e5d487dbe47d123051a44f65dfc773150cf3
Mason Ellwood <masonellwood@gmail.com>
2017-06-10 18:54:45 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :name, :posts
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  def posts
+    @posts
+  end
+  # def add_post(post)
+  #   @posts << post
+  #   post.author = self
+  # end
+end
