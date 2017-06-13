88e6ba273673f19fb0b2c3a5b98adf1099419509
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-13 03:49:54 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+require "pry"
+
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    self.author != nil ? self.author.name : nil
+  end
+
+end
