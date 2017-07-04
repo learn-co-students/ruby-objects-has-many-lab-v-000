23d8f168ada9a984c44a273c25314ff54bfd6681
Sydney Chun <schun08@gmail.com>
2017-07-04 15:17:14 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,21 +1,49 @@
-class Author
-  attr_accessor :name, :posts, :post_count
-  @@post_count = 0
+# first attempt 6/25/17
+# class Author
+#   attr_accessor :name, :posts, :post_count
+#   @@post_count = 0
+#
+#   def initialize(name)
+#     @name = name
+#     @posts = []
+#   end
+#
+#   def add_post(post)
+#     @posts << post
+#     post.author = self
+#     @@post_count += 1
+#   end
+#
+#   def add_post_by_title(title)
+#     post = Post.new(title)
+#     add_post(post) #you don't need self here?
+#   end
+#
+#   def self.post_count
+#     @@post_count
+#   end
+# end
 
+# second attempt 7/4/17
+
+class Author
+  attr_accessor :name, :posts
+  @@post_count = 0
+  
   def initialize(name)
     @name = name
     @posts = []
   end
 
   def add_post(post)
-    @posts << post
+    posts << post
     post.author = self
     @@post_count += 1
   end
 
   def add_post_by_title(title)
-    post = Post.new(title)
-    add_post(post) #you don't need self here?
+    new_post = Post.new(title)
+    self.add_post(new_post)
   end
 
   def self.post_count
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,21 @@
+# first attempt 6/25/17
+# class Song
+#   attr_accessor :name, :artist
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def artist_name
+#     self.artist == nil ? nil : self.artist.name
+#   end
+#
+# end
+
+# second attempt 7/4/17
+
 class Song
+
   attr_accessor :name, :artist
 
   def initialize(name)
@@ -6,7 +23,7 @@ class Song
   end
 
   def artist_name
-    self.artist == nil ? nil : self.artist.name
+    self.artist ? self.artist.name : nil
   end
-
+  
 end
