6f66cfcb7ba9d35d4ef71335a03ed9a49402969f
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-07-21 14:41:19 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+     attr_accessor :author
+     
+     def author=(author)
+          
+     end
+     
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+     attr_accessor :songs , :artist
+     
+     def initialize(name)
+          @name = name
+     end
+     
+     def artist_name
+     end
+     
+end
