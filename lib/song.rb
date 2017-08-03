35e6c6094bf37e474589623e49c7c06138490da4
Joanna Sese <jmgsese@gmail.com>
2017-08-03 17:54:15 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,12 @@ class Song
   end
 
   def artist_name
-    if @artist == nil
-      nil
-    else
-      self.artist.name
-    end
+    self.artist ? self.artist.name : nil
+    # if @artist == nil
+    #   nil
+    # else
+    #   self.artist.name
+    # end
   end
 
 end
