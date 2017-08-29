df5b90899d0cb74ca2880b69e4d283d3178fc61a
Sam Ojling <s.ojling@gmail.com>
2017-08-29 07:57:23 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if artist.nil?
+      nil
+    else
+      artist.name
+    end
+  end
+
+end
