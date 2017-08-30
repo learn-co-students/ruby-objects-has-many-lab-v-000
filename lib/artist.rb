200ad2776fde7afa96d17b6b1798a0197be9a865
Alyssa Sidebottom <alyssa.m.sidebottom@gmail.com>
2017-08-30 19:08:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  @@song_count = 0
+  attr_accessor :name, :song
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count +=1
+  end
+
+  def Artist.song_count
+    @@song_count
+  end
+
+end
