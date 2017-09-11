f12922cf92c5f216f783701732a32d397751c668
Chris Nady <nady.christopher@gmail.com>
2017-09-11 01:20:26 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize(title="Placeholder")
+    @title = title
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else
+      self.author.name
+    end
+  end
+
+end
