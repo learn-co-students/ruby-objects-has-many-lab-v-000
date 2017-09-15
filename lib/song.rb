513f6a0bb0b60e20a81edf5794562f34eca86b65
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-15 02:51:33 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :name, :artist
+  def initialize(name)
+    @name = name
+    @artist = self.artist
+  end
+
+  def artist_name
+    if defined? self.artist.name
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
