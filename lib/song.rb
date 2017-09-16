a80ed2dfb2b63fb75ef3cb6163949e4946f3dcc9
matos89 <matos89@gmail.com>
2017-09-16 08:49:12 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist
+    self.artist = @artist
+  end
+
+  def artist_name
+    if self.artist != nil
+      self.artist.name
+    else
+      nil
+    end
+  end
+end
