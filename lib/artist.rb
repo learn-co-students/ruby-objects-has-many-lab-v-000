3cf7f67ab4dbd696aa97a0e8ae90b9ecc3538f74
mevanstron <michael@michaelgregoryevans.com>
2017-09-29 18:14:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+
+  @@song_count = 0
+
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
