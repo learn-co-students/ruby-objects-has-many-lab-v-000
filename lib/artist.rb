86b2476d3a8810e946b828305f2c94b81a77ee66
Andrea Nasuto <andrea.nasuto@gmail.com>
2017-10-01 01:50:38 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+attr_accessor :name, :songs, :artist, :song_count
+@@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+    song
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
