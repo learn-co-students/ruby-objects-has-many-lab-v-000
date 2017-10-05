65c6c36504ff77e644f2fac657be62a7d108df77
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-05 12:46:29 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+attr_accessor :name, :artist
+
+  def initialize(name)
+    @name=name
+  end
+
+  def artist_name
+    if @artist
+      self.artist.name
+    else
+      self.artist
+    end
+  end
+
+end
