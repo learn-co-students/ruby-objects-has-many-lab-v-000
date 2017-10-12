7a55c770811800413a77d42f8e25afd1e9acbaf7
Charlie Max <dherzl@protonmail.com>
2017-10-12 17:57:05 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+  attr_accessor :name
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post_name)
+    # song = Song.new(post_name)
+    # add_song(song)
+    add_post(Post.new(post_name))
+  end
+
+  def posts
+    @posts
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
