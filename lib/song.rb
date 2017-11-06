201ea414244b7cf6d7f7b7eb52cf147327fe326f
Christopher DiStasio <github email address>
2017-11-05 19:08:55 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Artist
+
+    attr_accessor :name, :song
+
+    def initialize(name)
+        @name = name
+
+        @song = []
+
+    end
+
+    def add_song(song)
+        @song << song
+
+        self.song.Artist
+
+    end
+
+    def add_song_by_name(name)
+        @song
+        self.name.song.Artist
+    end
+
+    def song_count
+        @@song.count = song.artist.count
+        unless song.artist == nil
+
+        @@song.count
+        
+        end
+    end
+
+end
+
+        
+        
