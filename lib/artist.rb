79b03a5f118365707a49a55db9e4847121794e51
charityyoungblood <youngblood.charity@gmail.com>
2017-11-27 17:37:47 -0800

    added comments on how to approach project

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,13 @@
 require 'pry'
+
+#create Artist class 
+class Artist 
+    attr_accessor :name # create attr_accessor for name
+    def initialize(name) # initialized instance with name 
+        @name = name 
+        @songs = [] # create songs instance variable
+        # set to empty array
+    end 
+    
+    
+end 
