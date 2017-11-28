e4e20e71c89c810d892546fba14e2d32eec824ec
Charity <Youngblood.charity@gmail.com>
2017-11-28 13:05:49 -0800

    added comments on how to structure project in author.rb

---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+# create class Author 
+
+# create attr_accessor for name, posts
+
+# create class variable @@post_count
+
+# create initialize method with argument (name)
+
+# add collection of posts equal to an empty array
+
+# create name method 
+
+# create add_post method with argument (post)
+# add_post method should add the post to the author's collection (@posts array)
+# add_post method should show relationship between post and author 
+
+# create add_post_by_title method w/argument (post_title)
+
+# method above should create a new post
+
+# show relationship between post and author 
+
+# create class method (self.post_count)
+
+# method should return total #/posts
+
