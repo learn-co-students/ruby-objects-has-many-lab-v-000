1c6fdefa1ac6b9c322390c3e6a2bb24e921a6d98
charityyoungblood <youngblood.charity@gmail.com>
2017-11-27 17:57:10 -0800

    added attr_accessor for name and artist to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
 # create class Song 
 # initialize with argument of (name) 
 
-class Song 
+class Song
+    attr_accessor :name, :artist
+    def initialize(name)
+    end 
     
+    def name
+        @name
+    end 
+        
     
 end 
