e452c31744422c1614b95effce9e5bf06478fd88
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 14:02:52 -0800

    added attr_accessor to post.rb

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,14 @@
 # create class Post  
 class Post
+  attr_accessor :title
 # create initialize method with argument (title) 
   def initialize(title)
     
   end
 # create title method 
-
+  #def title 
+    
+  #end
 # create author method 
 
 # create author_name method 
