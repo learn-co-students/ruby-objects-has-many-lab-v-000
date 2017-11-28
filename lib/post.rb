3252adb8700f9bf63162beb109c2853730bb1f8c
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 14:00:00 -0800

    aded class Post and initialize method to post.rb

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,14 @@
-# create post Class 
-
+# create class Post  
+class Post
 # create initialize method with argument (title) 
-
+  def initialize(title)
+    
+  end
 # create title method 
 
 # create author method 
 
 # create author_name method 
 
+  
+end 
