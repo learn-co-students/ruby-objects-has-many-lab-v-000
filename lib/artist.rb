9c82712aa4cdc2c6666c4e28bf2d96cedff5d38b
charityyoungblood <youngblood.charity@gmail.com>
2017-11-27 17:47:00 -0800

    added comments for add_song method

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,18 @@ require 'pry'
 
 #create Artist class 
 class Artist 
-    attr_accessor :name # create attr_accessor for name
+    attr_accessor :name, :songs # create attr_accessor for name, songs
     def initialize(name) # initialized instance with name 
         @name = name 
         @songs = [] # create songs instance variable
         # set to empty array
     end 
     
+    # create add_song instance method 
+    # has one argument (song) 
+    
+    def add_song(song)
+    
+    end
     
 end 
