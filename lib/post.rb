7d01e887ba27c16799ccf0cbb7e519c4420c87cb
Yina Zhu <yinagit@gmail.com>
2017-12-03 22:13:26 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+
+  end
+
+def post
+  @post = post
+end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+  end
+end
+end
