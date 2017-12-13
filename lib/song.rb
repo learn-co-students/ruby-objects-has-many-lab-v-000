d8e687cab5a0d55180e8bcf1fdd91ffabd014b8c
Frank McCallion <keith@sensortech.com>
2017-12-13 13:05:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if self.artist != nil
+      self.artist.name
+    else
+      nil
+    end
+    #binding.pry
+  end
+
+end
