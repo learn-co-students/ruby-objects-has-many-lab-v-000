4ed23fa866436ca18c1f4e08e5ee985b32cbaa90
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-19 04:07:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+
+attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+    # @artist = artist
+  end
+
+  def artist_name
+    if self.artist != nil
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+
+
+end
