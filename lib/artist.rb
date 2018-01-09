5196053c78fd31f0ceeeb14ffac6cad49c4bbe70
Brad Hambrice <hambrice@uab.edu>
2018-01-09 07:08:32 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,9 +2,11 @@ require 'pry'
 
 class Artist
   attr_accessor :name 
+  @@song_count = 0 
   def initialize(name)
     @name = name
     @songs = []
+    
   end
   def songs
     @songs
@@ -13,12 +15,15 @@ def add_song(song)
   @songs << song 
   song.artist = self
   song
+  @@song_count += 1 
 end
-def add_song_by_name(song)
+def add_song_by_name(name)
   song = Song.new(name)
   @songs << song
-  song.artist = self
-  binding.pry 
-  song
+  song.artist = self 
+  @@song_count += 1 
+end
+def self.song_count
+  @@song_count
 end
 end
