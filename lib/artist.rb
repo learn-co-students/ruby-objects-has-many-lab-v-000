d218b27f5a582b358ccb1f19ab6396b6a6f813ce
Brad Hambrice <hambrice@uab.edu>
2018-01-09 05:25:30 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  def songs
+    @songs
+end
+def add_song(song)
+  @songs << song 
+  song.artist = self
+  song
+end
+def add_song_by_name(song)
+  song = Song.new(name)
+  @songs << song
+  song.artist = self
+  song
+end
+end
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :name, :artist
+  def initialize(name)
+    @name = name
+  end
+  def artist_name
+    if artist
+      return self.artist.name 
+    else
+      return nil 
+    end
+  end
+end
