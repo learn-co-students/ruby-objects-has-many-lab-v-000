b0863ecdce630c07d19705081331d9eca3eb886d
Ingrid Wong <yw340@georgetown.edu>
2018-01-18 20:20:57 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,36 @@
-def Artist
+require "pry"
+
+class Artist
+  @@song_count = 0
+  attr_accessor :name, :songs
+ def initialize(name)
+   @name = name
+   @songs = []
+ end
+
+ def self.song_count
+   @@song_count
+ end
+
+
+
+ def add_song(song)
+   self.songs << song
+   @@song_count += 1
+   song.artist = self
+ end
+
+ def add_song_by_name(song_name)
+   song = Song.new(song_name)
+   self.add_song(song)
+ end
+
+
+
 
   # use an instance variable @songs to build the "has many relationship"
   # add each song by name
     #create then push
-  # 
+  #
 
 end
