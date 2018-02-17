1cbf8cbd0f45f57ddabb757bfedd4d1266c4cd3e
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-17 19:29:03 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  attr_accessor :title, :author
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def author
+    @author
+  end
+  
+  def author_name
+    if @author == nil
+      nil
+    else
+      self.author.name
+    end
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def artist
+    @artist
+  end
+  
+  def artist_name
+    if @artist == nil
+      nil
+    else
+      self.artist.name
+    end
+  end
+  
+end
