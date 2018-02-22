07c1aaa9a7b3649efc6da21bee5da70a78157d5e
Jayon Kim <jpkim921@gmail.com>
2018-02-22 03:05:00 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name, :songs
 
- @@song_count = []
+ @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -12,15 +12,19 @@ class Artist
   def add_song(song)
     songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name_of_song)
     song = Song.new(name_of_song)
     songs << song
     song.artist = self
+    @@song_count += 1
   end
 
-  # def song_count
-  #
-  # end
+
+  def self.song_count
+    @@song_count
+  end
+
 end
