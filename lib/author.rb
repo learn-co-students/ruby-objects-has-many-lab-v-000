881f17daa6a8c59519b0d1deac072c6b2e7a6829
Chrissa Constantine <chrissa1@mac.com>
2018-02-22 02:03:21 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,35 @@
+class Author
+
+attr_accessor :name, :posts, :post_count
+
+@@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(title)
+    self.posts << title
+    title.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(name)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  
+
+  def self.post_count
+    @@post_count
+  end
+
+end
