ba9a6440c4b9ca9b935310320376a7603452c847
Aderianna T Williams <aderiannaw@yahoo.com>
2018-03-08 05:25:03 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name, :songs
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+      @songs << song
+       @@song_count += 1
+      song.artist = self
+    
+  end
+  
+  def self.song_count
+    @@song_count
+  end
+end
