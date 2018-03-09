6bb4af5ea70dff808e8cf3bbfae38330a0d21eab
Beth Schofield <bethmschofield@gmail.com>
2018-03-09 03:42:19 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name, :songs, :genre
+  @@song_count = 0
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+  def add_song_by_name(songtitle)
+    newsong = Song.new(songtitle)
+    @songs << newsong
+    @@song_count += 1
+    newsong.artist = self
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
