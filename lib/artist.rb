7ffc64ab5e936372fdfa3cd73e8973d569f5f69c
Aderianna T Williams <aderiannaw@yahoo.com>
2018-03-09 03:37:53 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,10 +8,17 @@ class Artist
   end
   
   def add_song(song)
-      @songs << song
+        @songs << song
        @@song_count += 1
       song.artist = self
     
+  end
+  
+   def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
   end
   
   def self.song_count
