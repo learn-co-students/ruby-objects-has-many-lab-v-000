a4a87ed0adcb2b903be999e5a43f20bb319d015a
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-14 19:54:29 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,30 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :posts
+  @@all = []
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+    @@all << self
+  end
+
+  def add_post(post)
+    posts << post
+    post.artist = self
+    @@post_count += 1
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    add_post(post)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.post_count
+    @@post_count
+  end
 end
