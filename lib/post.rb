42d077521944d9db79953060dc5ff9020ab6d358
Karen G <kgraves0701@gmail.com>
2018-04-03 03:00:53 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  attr_accessor :title, :author
+
+  @@post_count = 0
+
+  def initialize(title)
+    @title = title
+  end
+
+
+  def author_name
+    if self.author
+     self.author.name
+    else nil
+    end
+  end
+
+
+end
