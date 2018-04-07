514da6fa9d59f57b7ac3eb57ca8306a76bba4b9c
Eric Johnson <ericjohnson0216@icloud.com>
2018-04-07 03:17:32 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,16 @@
-class Post
 
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
 end
