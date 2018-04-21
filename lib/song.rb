b6a21e7019b9b4bad4ad92f8d8eceac3ddc07c4d
Erica Murdock-Waters <ericahpa@gmail.com>
2018-04-21 18:04:17 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+# song.rb
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if self.artist = artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+end
