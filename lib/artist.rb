38a9fbadf432f3f5d14123c4d826274c59afa9bb
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-09 02:18:23 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,16 +11,18 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-
+    @@song_count
   end
 end
 
