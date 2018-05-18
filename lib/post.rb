e1a20209e0c8ab43f8ae7cea0e5a4e99cde42245
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-18 20:36:11 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+  attr_accessor :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author
+    @author
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+
+
+end
