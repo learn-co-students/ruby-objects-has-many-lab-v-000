024301baeb27f8516e16cd0b050efc32f346e1fc
Shirley Collins <shirley@m104.us>
2018-05-20 08:16:09 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist
+  @@song_count = 0
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @@song_count += 1
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    @@song_count += 1
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
