95ac5d24fcd0d9b1ea331ca13a43d141b4b60628
Dale Terrell <tragicid@gmail.com>
2018-05-21 19:15:57 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+  attr_accessor :name, :author 
+
+  @@all = []
+
+  def initialize(name, author=nil)
+    @name = name
+    @author = author
+    @@all << self
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
+
---
+++ lib/post.rb
@@ -0,0 +1,24 @@
+class Post
+  attr_accessor :name, :posts
+
+  @@all = []
+
+  def initialize(name, author=nil)
+    @name = name
+    @author = author
+    @@all << self
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
+
