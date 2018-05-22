689a2426ce27211034ceb09365a2664f0387d6f3
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-22 19:55:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+
+class Artist
+  attr_accessor :name, :songs
+  @@song_count = 0
+  
+  def initialize(artist)
+    @name = artist
+    @songs = []
+  end
+  
+  def self.song_count_up
+    @@song_count += 1
+  end
+  
+  def self.song_count
+    @@song_count
+  end
+  
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    Artist.song_count_up
+  end
+  
+  def add_song_by_name(song_name)
+    new_song = Song.new(song_name)
+    self.add_song(new_song)
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+
+class Song
+  attr_accessor :name, :artist, :artist_name
+  
+  def initialize(name)
+    @name = name
+  end
+
+
+end
