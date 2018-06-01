fc70e4c6aff6d3fa599f93804c12362b52dda2ba
Lamia Haider <lamia.haider@gmail.com>
2018-06-01 02:33:59 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize (title)
+    @title = title
+    @author = author
+    @@all << self
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    end
+  end
+
+
+
+end
