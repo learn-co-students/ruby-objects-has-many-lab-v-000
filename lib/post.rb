0ca4e00aad1ca45d7d5b91b04cf5ff85605cc1c2
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-18 15:34:23 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+  attr_accessor :title, :author
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    @author.name
+  end
+
+end
