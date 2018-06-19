e945a5c966b2ebefc85962b4117db65712d40ea1
Brandon Green <brandon@brandonleeproductions.com>
2018-06-19 04:17:44 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+# Spent too much time on this one.... I'll come back to this another time.
+
+require_relative 'song'
+
+class Artist
+  attr_accessor :name, :artist, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name, @songs = name, []
+  end
+
+  def add_song(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    self.author.name
+  rescue
+    nil
+  end
+
+end
