34c131305c2360d88c0982f8c3c51d6306d276cb
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-21 16:08:54 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+end
