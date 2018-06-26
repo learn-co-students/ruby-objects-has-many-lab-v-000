dfa845ecb9ff63808f6e45e1f70e4b68772cb89d
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-26 16:34:49 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def artist_name
+end
+  def author_name
+  end
+
+  def add_song_by_name
+end
+def add_post_by_title
+end
+
+
+end
+
