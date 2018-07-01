0b41cf3521c391b05a964c19a91ce6b1ac762a0e
Edsel Caprice <caprice@mail.usf.edu>
2018-07-01 21:26:58 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+attr_accessor :name, :artist
+
+@@all = []
+def initialize(name)
+@name = name
+@@all << self
+end
+
+def self.all
+@@all
+end
+
+def artist_name
+self.artist if self.artist == nil
+self.artist.name if self.artist != nil
+
+end
+
+
+end
