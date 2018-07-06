9d085592bbaed849530b9ceed26c3e085e5edb14
Ethel C <cofresi.ethel@gmail.com>
2018-07-06 17:23:47 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+  attr_accessor :name, :posts, :author
+  
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+      @posts
+  end
+  
+  def add_post(input_post)
+    @posts << post
+    input_post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    title = Post.new(title)
+    title.author = self
+    @posts << title
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
