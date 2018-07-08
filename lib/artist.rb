e88725ff4cdb82b34cb3ab72479d1b65468d481c
Adam Barton <adam.barton333@gmail.com>
2018-07-08 05:19:08 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Artist
+
+  attr_accessor :name
+
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
+
+    def add_song(song)
+      song.artist = self
+      @songs << song
+    end
+
+    def songs
+      @songs
+    end
+
+    def add_song_by_name(name)
+      song = Song.new(name)
+      @songs << song
+      song.artist = self
+    end
+
+    def self.song_count
+      @songs.each {|s| s.count}
+    end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+  attr_accessor :name, :genre, :artist
+
+  @@all_songs = []
+
+  def initialize(name)
+    @name = name
+  #  @@all_songs << song
+
+  end
+
+
+end
