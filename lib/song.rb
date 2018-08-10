a9de27ce3d741aab7c83e7697740cb57e8f2aa48
Anna Dybas <a.m.dybas@gmail.com>
2018-08-10 19:37:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,19 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
   end
 
 end
