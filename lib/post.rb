178ded502f5645dbc7bfbcc0b5b6a666489f41a5
Eli Cho <eli.cho@gmail.com>
2018-08-11 05:24:20 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  
+  @@all = []
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
