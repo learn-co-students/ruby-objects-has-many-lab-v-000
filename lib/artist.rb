d0c9a5806293d134f7a383774aa466441df91f6e
Eli Cho <eli.cho@gmail.com>
2018-08-11 05:54:32 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
 
   attr_accessor :name, :songs
 
-  # @@song_count = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -12,7 +12,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    # @@song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
@@ -23,7 +23,7 @@ class Artist
   end
 
   def self.song_count
-    @songs.size
+    @@song_count
   end
 
 end
