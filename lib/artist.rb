8491bc3931a54ddb4ddce7aeec453698a2a0c2b4
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-21 18:08:16 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
