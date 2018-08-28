49c0ceb12395cb973b8784937d04ac61a2f9fbc9
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 17:41:49 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+  
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end 
+  
+  def author_name
+    self.author.name if self.author
+  end 
+  
+  
+end 
