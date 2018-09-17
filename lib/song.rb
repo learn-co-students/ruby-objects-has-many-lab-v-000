7b7e3a7ec361116ddb8097226669f1ca34306571
John Blank <veloblank@gmail.com>
2018-09-17 09:19:28 -0400

    Redo lab. Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,23 @@
 class Song
   attr_accessor :artist, :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    new
+  end
+
+  def new
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-
+    if @artist
+      @artist.name
     end
-
   end
-
 end
