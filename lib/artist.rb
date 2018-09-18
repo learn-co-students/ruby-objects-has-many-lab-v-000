d53af0e15635a21ba7581b93976ae06d28ba6d3d
Ashley Wong <ashwong04@gmail.com>
2018-09-18 12:22:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,8 +7,20 @@ class Artist
   end
   
   def add_song(song)
+    song.artist = self
     @songs << song
   end
+  
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    @songs << song_name
+    song_name.artist = self
+  end
+  
+  def song_count
+      
+  end
+  
   
   
 end
