874eead986989a23b5cfff6521dd441c7d941a54
Ashley Wong <ashwong04@gmail.com>
2018-09-18 12:35:40 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_accessor :name, :posts
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+  
+  
+end
