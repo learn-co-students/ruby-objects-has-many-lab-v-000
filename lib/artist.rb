fe7f721958bb35709cee04974415d74a3afd87b1
Sherry Martin <sm674@cornell.edu>
2018-09-25 12:49:07 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,19 @@
 class Artist 
   attr_accessor :name, :songs 
   
-    @@songs = []
-  
   def initialize(name)
     @name = name
     @songs = []
   end
   
   def add_song(song)
-    song.artist = self
+    @songs << song
+   song.artist = self
+   Song.new(self)
+  end
+  
+  def songs
+    @songs
   end
   
   def add_song_by_name(name)
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def artist_name
+    self.artist.name
+  end
+end
