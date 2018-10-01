087ccd61de2d5008615ffa96480a6627c04cdb28
Cheryl K <cherylkelson@icloud.com>
2018-10-01 22:49:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist  
+  attr_accessor :name 
+  
+  def initialize(name)
+   @name = name  
+   @all = self 
+  end
+  
+  
+  
+  
+  # def songs  
+    
+  # end 
+  
+  
+  # def add_song   
+    
+  # end 
+   
+   
+  # def add_song_by_name  
+     
+  # end  
+   
+   
+  # class .song_count 
+end
+
+
+# class Artist  
+#   attr_accessor :name@@all = []
+  
+#   def initialize(name) 
+#     @name =  name 
+#     @@all = self 
+#   end 
+  
+#   def add_Song(song)
+#     song.artist= self    # song.artist = self vs song.artist=self 
+#   end 
+  
+  
---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+# class Song  
+#   attr_accessor :name,  :artist  
+#   @@all = [] 
+  
+#   def initialize(name) 
+#     @name = name 
+#     @@all << self 
+#   end 
+  
+#   def add_song(song) #argument is instance of a song 
+#     song.artist = self    #artist setter methos is equal to elf whidh is artist instance
+#   end 
+
+
+#   def self. all   
+#     @@all       # to return the array 
+#   end 
