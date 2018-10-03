d5c39a301d5184c862ec8ac68e7551cab8edf106
J Fern <jfern20@gmail.com>
2018-10-03 01:04:44 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist 
+    attr_accessor :name
+   
+    def initialize(name)
+      @name = name 
+     @songs = []
+    end 
+  
+   def songs  
+     @songs 
+   end 
+   
+   def add_song(song)
+     
+   end 
+  
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song 
+   attr_accessor :name, :artist 
+   def initialize(name)
+     @name = name
+      @all = []
+   end 
+  
+  def all 
+    @all 
+  end 
+  
+end 
