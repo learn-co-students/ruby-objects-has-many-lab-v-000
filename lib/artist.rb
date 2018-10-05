27d65d7ba1b2814daedf49e50111d97792bea95e
Erin Green <erin.y.green@gmail.com>
2018-10-05 01:32:46 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require "pry"
+
+class Artist 
+  
+  attr_accessor :name, :songs 
+  
+  @@songs = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+     
+  end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+    @@songs << self
+  end 
+  
+  def self.songs 
+    @songs
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    song.artist = self 
+    @@songs << self
+  end
+
+  def self.song_count 
+    @@songs.length 
+  end 
+
+end
