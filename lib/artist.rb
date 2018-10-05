c2d96fab5c6f589869f5dac95ac24c653b10bb7e
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-05 07:02:50 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+
+
+class Artist
+  attr_accessor :name, :songs
+  @@songs = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @@songs << song
+    @songs << song
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @@songs << song
+    @songs << song
+    song.artist = self
+  end
+
+  def self.song_count
+    @@songs.count
+  end
+
+end
