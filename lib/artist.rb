15cc25c616b05f1526fd4dcfe66897e42fb410d5
Brian Birdwell <bbirdwell@gmail.com>
2018-10-05 19:14:12 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+  attr_accessor :name
+  
+  @@songs = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@songs << song
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def add_song_by_name(name)
+    song  = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@songs << song
+  end
+
+  def self.song_count
+    @@songs.length
+  end
+
+end
