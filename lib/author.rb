ebd88e4b3bdb676d2e745726d720777b9f799d72
Fatama Rahman <fatamarahman@gmail.com>
2018-10-07 07:12:28 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+
+  attr_accessor :name
+
+  @@posts = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    @@posts << post
+    post.author = self
+  end
+
+  def posts
+    @@posts
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    @@posts << new_post
+    new_post.author = self
+  end
+
+  def self.post_count
+    @@posts.count
+  end
+
+end
