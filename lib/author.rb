79360dad5b067c194e3b5bbd4178f28d3b081b49
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-11 02:50:54 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Author
-  attr_accessor :name
+  attr_accessor :name, :post, :author, :title
+ 
+  @@posts = []
+ 
+  def initialize(name)
+    @name = name
+    @post = post 
+  end
+ 
+  def add_post(post)
+    @@posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @@posts << post
+    post.author = self
+    title
+    #binding.pry
+  end
+ 
+  def posts
+    @@posts
+  end
+  
+  def self.post_count
+    @@posts.count
+  end
+
 end
