181b1f1e9ae54d6c9d60a4a9006862947175ebb1
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-02 20:03:45 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_accessor :name, :songs
+  
+  def initialize(name)
+    @name=name
+  end
+  
+  def add_song(song)
+    
+  end
+  
+  def add_song_by_name(song)
+    
+  end
+  
+  def self.song_count
+    
+  end
+  
+end
---
+++ lib/author.rb
@@ -0,0 +1,20 @@
+class Author
+  attr_accessor :name :posts
+  
+  def initialize(name)
+    @name=name
+  end
+  
+  def add_post(post)
+    
+  end
+  
+  def add_post_by_title(post)
+    
+  end
+  
+  def self.post_count
+    
+  end
+  
+end
