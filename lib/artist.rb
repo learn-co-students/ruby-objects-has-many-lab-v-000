0031ac5c013dd3268e99948a91289bbf37bfbda3
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-02 21:53:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,33 @@
+require 'pry'
 class Artist
   attr_accessor :name, :songs
+  @@all = []
   
   def initialize(name)
     @name=name
+    @songs=[]
   end
   
   def add_song(song)
-    
+    @songs << song
+    song.artist = self
+    @@all << song
   end
   
-  def add_song_by_name(song)
-    
+  def add_song_by_name(name)
+    song=Song.new(name)
+    @songs << song
+    song.artist = self
+    song.name = name
+    @@all << song
+  end
+  
+  def songs
+    @songs
   end
   
   def self.song_count
-    
+    @@all.count
   end
   
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,20 @@
+require 'pry'
 class Song
+  attr_accessor :name, :artist
+  @@all=[]
   
+  def initialize(song)
+  binding.pry
+  @song = song
+  @@all << self
   
+end
+
+  def artist_name
+    self.artist.name
+  end
+  
+  def self.all
+    @@all
+  end
 end
