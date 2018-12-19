562b3d824308a4e87408535964eeea3cef1659b3
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-19 20:49:49 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,8 @@ class Artist
   attr_accessor :name, :artist
   attr_reader :songs
   
+  @@song_count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -12,11 +14,16 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self #reciprocity
+    @@song_count += 1
   end
     
   def add_song_by_name(songName)
     song = Song.new(songName)
     self.add_song(song)
+  end
+  
+  def self.song_count
+    @@song_count
   end
   
 end
