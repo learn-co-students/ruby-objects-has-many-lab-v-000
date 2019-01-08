cfff314def495d01d222b73c53c6030f230286ae
Arielle D <domingu4@gmail.com>
2019-01-08 15:06:26 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(name)
+    post = Post.new(name)
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
