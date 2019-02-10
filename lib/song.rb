a75cda19f4a66a1c8e29f3d1c0148060e90460e7
Kimberly Aviles <01010110.v@gmail.com>
2019-02-10 21:37:35 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :artist, :name
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end #<---- class end
