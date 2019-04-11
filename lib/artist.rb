ae8d316d82efe59c3b06ce4da734cc30018b591e
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-11 19:10:38 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    @songs = []
+  end
+
+  def add_song(song)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+end
