c2bae80ea7ea7db587f5cdb749b4f3d8e331c3ca
Angie Goza <angielg9888@gmail.com>
2019-04-17 22:32:04 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+class Author
+  attr_accessor :name
+  @@post_count = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    @posts
+  end
+  
+  def add_post(title)
+    title.author = self
+    @posts << title
+    @@post_count << title
+  end
+  
+  def add_post_by_title(title)
+    title = Post.new(title)
+    title.author = self
+    @posts << title
+    @@post_count << title
+  end
+  
+  def self.post_count
+    counter = 0
+    @@post_count.each do |post|
+      counter += 1
+    end
+    counter
+  end
+end
