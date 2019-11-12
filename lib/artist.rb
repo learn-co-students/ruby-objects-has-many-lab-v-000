b1810e8f2c279a9908eeca2f4aa76c3955f6e6f3
Eva Navarro <emanao@me.com>
2019-11-12 16:36:01 +0000

    Use @@all class variable pattern. Artist patch for old test spec

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,36 @@
 class Artist
-  attr_accessor :name, :songs
-  @@song_count=0
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
-    @name=name
-    @songs=[]
+    #TBD: instance variable needed to fullfill the test expectations. Not needed.
+    @songs = []
+
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def add_song(song)
-    @songs<<song
-    song.artist=self
-    @@song_count+=1
+    song.artist = self
   end
 
-  def add_song_by_name(title)
-    song=Song.new(title)
-    @songs<<song
-    song.artist=self
-    @@song_count+=1
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def self.song_count
-    @@song_count
-  end
+    #TBD: class variable needed to fullfill the test expectations. Not needed.
+    @@song_count = Song.all.count
 
+  end
 end
